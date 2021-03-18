939170c465aa94c55e5a598eb174467d5b813f5c
Mckupe <65507746+Mckupe@users.noreply.github.com>
2021-03-18 20:00:59 +0500

    20:00
    ses

--- DashRouge/Assets/Scripst/DashMove.cs
+++ DashRouge/Assets/Scripst/DashMove.cs
@@ -5,16 +5,13 @@ using System.Collections.Generic;
 
 public class DashMove : MonoBehaviour
 {
-    private Rigidbody rb;
-    public Camera cam;
-    public float maxSwipeLenght;
-    Vector3 touchPos;
-    Vector3 releasePos;
-    Vector3 swipeVector;
-    void Start()
+    public float maxSwipeLenght, speed;
+    Vector3 touchPos, releasePos, swipeVector, seckondPoint;
+    private Rigidbody rbody;
+    bool nigger;
+    private void Start()
     {
-        cam = Camera.main;
-        rb = GetComponent<Rigidbody>();
+        rbody = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -50,16 +47,23 @@ public class DashMove : MonoBehaviour
                         releasePos = hit.point;
                     }
                     releasePos.y = 0;   
-                    swipeVector = releasePos - touchPos;   //movement vector
-                    transform.position += Vector3.ClampMagnitude(swipeVector, maxSwipeLenght);
+                    swipeVector = releasePos - touchPos;//movement vector
+                    seckondPoint = transform.position + Vector3.ClampMagnitude(swipeVector, maxSwipeLenght);
                     Debug.Log("release at" + releasePos);
                     break;
-            }
+            }        
+        }    
+    }
+    public void FixedUpdate()
+    {
+        Vector3 postMove = Vector3.Lerp(transform.position, seckondPoint, speed);
+        transform.position = postMove;
+    }
+    public void OnCollisionEnter (Collision col)
+    {
+        if (col.collider.name == "wall")
+        { 
+
         }
     }
-    //private void FixedUpdate()
-    //{
-    //        Vector3 postMove = Vector3.Lerp(transform.position, transform.position += swipeVector, 0.125f);
-    //        transform.position = postMove;
-    //}
 }
